a71f28d3cc09e8c5e9d58586800358d33afbedb4
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-22 15:34:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 100) + 1
+ cart.push({item : price})
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
