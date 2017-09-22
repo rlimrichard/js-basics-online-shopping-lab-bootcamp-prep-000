9b01b860cdbb72b55f3ae1a5ff76e70f3aeb992e
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-22 16:06:45 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -117,7 +117,7 @@ describe("viewCart()", function() {
     const pearCost = getCart()[1]["pear"];
     const quinceCost = getCart()[2]["quince"];
 
-    viewCart();
+    console.log(viewCart());
 
     expect(console.log).toHaveBeenCalledWith(
       `In your cart, you have orange at $${orangeCost}, pear at $${pearCost}, and quince at $${quinceCost}.`
