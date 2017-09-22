d7d188dffd8967e8bc5b00bf87d3ed565cf44f27
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-22 20:19:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -105,5 +105,15 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+    if (cardNumber === undefined){
+        console.log("Sorry, we don't have a credit card on file for you.";
+    }
+    else{
+        console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    }
+    return cart;
+    
+    
+        
+  
 }
